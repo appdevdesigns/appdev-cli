ea4498b4f76918579c2a51eaafd1cfd9273846c5
Johnny Hausman <johnny@appdevdesigns.net>
2013-12-11 16:45:27 +0700

    fixture test working from scratch area.

--- test/appdev_fixture.js
+++ test/appdev_fixture.js
@@ -2,6 +2,7 @@
 var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
 var fs = require('fs');
+var path = require('path');
 var $ = require('jquery');
 
 var Util = require('./helpers/util_helper.js');
@@ -11,47 +12,43 @@ describe('test appdev fixture :applicationName :resource :fieldList ',function()
 
 	//Before the unit tests are executed, create the testing directory structure
 	before(function(done){
+	    this.timeout(10000);
 
-		//Set timeout to 11 secs 'cause this process takes longer than normal
-		this.timeout(41000);
+	    // run this command from the test/scratchArea/ directory
+	    process.chdir(path.join(__dirname, 'scratchArea'));
 
-		var setup = Util.testingEnvSetup();
-		$.when(setup)
-		.then(function(data){
-
-		    process.chdir('testApplication');
-
-		    // now run the command to create a fixture
-		    var finished = Util.spawn('appdev', ['fixture', 'testApplication', 'TestModel', 'model_field:string', 'model_field2:string', 'model_field3:integer'])
-		    $.when(finished)
-		    .then(function(data) {
-		        done();
-		    })
-		    .fail(function(err){
-		        done(err);
-		    })
-
-		})
-		.fail(function(err){
-		    done(err);
-		})
-	});
-
-	//After the tests are run, delete the testApplication directory
-	after(function(done){
-	    done();
-/*
-	    process.cwd('..');
-	    var removed = Util.testingEnvRemove();
-	    $.when(removed)
-	    .then(function(data){
+	    // now run the command to create a fixture
+	    var finished = Util.spawn('appdev', ['fixture', 'testApplication', 'TestModel', 'model_field:string', 'model_field2:string', 'model_field3:integer'])
+	    $.when(finished)
+	    .then(function(data) {
 	        done();
 	    })
 	    .fail(function(err){
 	        done(err);
 	    })
 
-*/
+	});
+
+	//After the tests are run, delete our fixture directories
+	after(function(done){
+
+	    // return to our top level directory
+	    process.chdir(path.join(__dirname, '..'));
+
+	    // remove /api & /assets directory
+	    var apiGone = Util.removeDir(path.join(__dirname, 'scratchArea', 'api'));
+	    var assetsGone = Util.removeDir(path.join(__dirname, 'scratchArea', 'assets'));
+	    $.when(apiGone, assetsGone).then(function() {
+
+	        // all good, continue
+	        done();
+	    })
+	    .fail(function(err){
+
+	        // dang!
+	        done(err);
+	    });
+
 	});
 
 
@@ -65,10 +62,43 @@ describe('test appdev fixture :applicationName :resource :fieldList ',function()
 
 	describe('check the TestModelController.js file has the proper data ', function(){
 
-	    it('the controller exists ',function(){
-	        var found = fs.existsSync("/tmp/testApplication/api/controllers/TestModelController.js");
-	        assert.isTrue(found,' yup, our controller exists.');
+	    var controllerPath = path.join(__dirname,'scratchArea', 'api', 'controllers', 'TestModelController.js');
+	    var controllerFile = '';
+
+	    before(function(){
+	        controllerFile = fs.readFileSync(controllerPath, 'utf8');
 	    });
+
+
+
+	    it(' -> the controller exists ',function(){
+	        var found = fs.existsSync(controllerPath);
+	        assert.isTrue(found,' => yup, our controller exists.');
+	    });
+
+
+
+	    it(' -> the controller has method find ',function(){
+            assert.isTrue((controllerFile.indexOf('find:function') != -1),' => find() method defined');
+        });
+
+
+
+	    it(' -> the controller has method create ',function(){
+            assert.isTrue((controllerFile.indexOf('create:function') != -1),' => create() method defined');
+        });
+
+
+
+	    it(' -> the controller has method update ',function(){
+            assert.isTrue((controllerFile.indexOf('update:function') != -1),' => update() method defined');
+        });
+
+
+
+	    it(' -> the controller has method destroy ',function(){
+            assert.isTrue((controllerFile.indexOf('destroy:function') != -1),' => destroy() method defined');
+        });
 	});
 
 
--- test/helpers/util_helper.js
+++ test/helpers/util_helper.js
@@ -14,17 +14,12 @@ module.exports= {
         spawn: function(command, options) {
             var dfd = $.Deferred();
 
-
-
             //Issue "appdev install testApplication" command
             cmd = spawn(command, options);
 
             //Listen for stdout messages
             cmd.stdout.on('data', function (data) {
-                console.log('%' + data);
-
-
-
+//                console.log('%' + data);
             });
 
             //Listen for stderr messages
@@ -38,14 +33,12 @@ module.exports= {
             cmd.on('error', function (err) {
                 console.log('err: '+err);
 
-//                if (done) done(err);
                 dfd.reject(err);
             });
 
             //Listen for closing
             cmd.on('close', function (code) {
-                console.log('child process exited with code ' + code);
-//                if (done) done();
+//                console.log('child process exited with code ' + code);
 
                 dfd.resolve();
             });
@@ -54,90 +47,12 @@ module.exports= {
         },
 
 
-        testingEnvSetup: function(done) {
+
+        removeDir: function(path, done) {
             var dfd = $.Deferred();
 
-            currentPath = process.cwd();
-
-            //Change directory to tmp to create application
-            process.chdir('/tmp');
-
-
-            var removed = this.testingEnvRemove();
-            $.when(removed)
-            .then(function(){
-
-
-                //Issue "appdev install testApplication" command
-                cmd = spawn('appdev',['install','testApplication']);
-
-                //Listen for stdout messages
-                cmd.stdout.on('data', function (data) {
-                    console.log('' + data);
-
-                    //Catch the prompting for input, then issue an return character in response
-                    if (data.toString().indexOf("db adaptor") != -1
-                       || data.toString().indexOf("connect by socket") != -1
-                       || data.toString().indexOf("localhost") != -1
-                       || data.toString().indexOf("port:") != -1
-                       || data.toString().indexOf("user:") != -1
-                       || data.toString().indexOf("password:") != -1
-                       || data.toString().indexOf("database:") != -1){
-                        cmd.stdin.write('\n');
-                    }
-
-
-                    if (data.toString().indexOf('sails lift') != -1) {
-                        // that should be the last of our msg,
-console.log('!!!!! sails lift found !!!!!');
-                        dfd.resolve();
-                    }
-
-                });
-
-                //Listen for stderr messages
-                cmd.stderr.on('data', function (data) {
-                    console.log('stderr: ' + data);
-
-                    // this was an error?
-                });
-
-                //Listen for error messages
-                cmd.on('error', function (err) {
-                    console.log('err: '+err);
-
-                    if (done) done(err);
-                    dfd.reject(err);
-                });
-
-                //Listen for closing
-                cmd.on('close', function (code) {
-console.log('!!!!! cmd.on.close !!!!!');
-                    console.log('child process exited with code ' + code);
-                    if (done) done();
-                    dfd.resolve();
-                });
-
-            //Wait to allow the program time to finish
-//            setTimeout(function(){
-//                done();
-//            },10000);
-
-            })
-
-            return dfd;
-        },
-
-
-
-        testingEnvRemove: function(done) {
-            var dfd = $.Deferred();
-
-          //After the test is run, remove the directory for the application
-            exec('rm -Rf testApplication',function(err,stdout,stderr){
-
-                // return us to our expected path.
-                process.chdir(currentPath);
+            // remove the indicated directory
+            exec('rm -Rf '+path,function(err,stdout,stderr){
 
                 if (err){
                     console.log("err = "+err);
