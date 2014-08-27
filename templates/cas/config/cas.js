332dcfd463f71466b0f4d8b7cc8a2fc4e03974c3
Johnny Hausman <johnny@appdevdesigns.net>
2014-08-27 11:26:15 +0700

    + added questions for CAS proxyURL setting

--- templates/cas/config/cas.js
+++ templates/cas/config/cas.js
@@ -4,6 +4,16 @@
 
 module.exports.cas = {
 
-    baseURL: 'https://signin.example.com:443/cas'
+	/*
+	 *  baseURL: the url to your CAS server.  
+	 */ 
+    baseURL: 'https://signin.example.com:443/cas',
+
+    /*
+     *  proxyURL: the url to your proxy server.
+     *
+     *  if you don't support Proxy Connections then just leave this 'undefined'
+     */
+    proxyURL: undefined
 
 };
