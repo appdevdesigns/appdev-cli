46fcdf7f0c2df5366473246497d41c5ba36d5572
Johnny Hausman <johnny@hausmanfamily.com>
2016-02-25 14:29:22 +0700

    + add: crudWebix now produces webix datatable with model loaded.

---
+++ templates/crudWebix/assets/[appName]/controllers/[ControllerName].js
@@ -0,0 +1,19 @@
+
+steal(
+        // List your Controller's dependencies here:
+        'appdev',
+//        '<%= appName %>/models/Projects.js',
+function(){
+
+    // Namespacing conventions:
+    // AD.Control.extend('[application].[controller]', [{ static },] {instance} );
+    AD.Control.extend('<%= correctControllerName %>', {  
+
+
+////crudWebixTemplateHere
+
+
+    });
+
+
+});
