4d92718ba93f2f0bc9543387b0357208d0bbeb2c
Johnny Hausman <johnny@hausmanfamily.com>
2016-03-25 11:54:24 +0700

    *fix: modifications to accommodate new_build changes.

--- templates/crudWebix/assets/[appName]/controllers/[ControllerName].js
+++ templates/crudWebix/assets/[appName]/controllers/[ControllerName].js
@@ -1,19 +1,26 @@
-
 steal(
-        // List your Controller's dependencies here:
-        'appdev',
-        '<%= clientResourcePath %>',
-function(){
+    // List your Controller's dependencies here:
+    '<%= clientResourcePath %>',
+    function() {
+        System.import('appdev').then(function() {
+            steal.import('appdev/ad',
+                'appdev/control/control').then(function() {
 
-    // Namespacing conventions:
-    // AD.Control.extend('[application].[controller]', [{ static },] {instance} );
-    AD.Control.extend('<%= correctControllerName %>', {  
+
+                // Namespacing conventions:
+                // AD.Control.extend('[application].[controller]', [{ static },] {instance} );
+                AD.Control.extend('<%= correctControllerName %>', {  
 
 
 ////crudWebixTemplateHere
 
 
+                });
+
+
+
+            });
+
+        });
+
     });
-
-
-});
