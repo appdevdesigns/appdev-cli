65e6190efd206726186a3a7e79425f58ee82689b
Nattawat Jaikaew <jnattawat@kiragames.com>
2016-04-11 09:31:06 +0700

    Test Add Setting File

---
+++ settings/settings.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    // DB Adaptor settings
+    adaptor:"mysql",            // which DB adaptor to use? 
+    connectionType: "port",     // connect by port
+    socketPath : "na",          // needs to be present, but not used.
+    host: "192.168.99.100",          // url of host
+    port: "3306",               
+    user: "root",
+    password: "root", 
+    database: "develop",
+
+    wantTest:"n",        // want test adaptor?
+
+    // SSL Settings:
+    enableSSL:false,    // no ssl
+
+
+    // Authentication
+    // authType:"local",
+    authType:"Local",
+    baseURL:"",
+    pgtURL:"",
+    guidKey:"",
+
+    // 
+    copyConfigs:true,
+    configDir:"settings/config"
+}
