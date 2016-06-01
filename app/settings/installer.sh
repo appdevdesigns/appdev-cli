1f4b908209c0ea189b0ae4b7d7d34bff3ef134cd
Nattawat Jaikaew <jnattawat@kiragames.com>
2016-06-02 03:37:30 +0700

    Add space

--- app/settings/installer.sh
+++ app/settings/installer.sh
@@ -1,4 +1,4 @@
-if [ ! -d "sails"]; then
+if [ ! -d "sails" ]; then
 	exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:../settings/settings.js
 else 
 	echo Please command sails lift!
