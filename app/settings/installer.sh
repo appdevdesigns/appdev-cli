aee935409142a1064fd3407334accdb4a92a6545
Nattawat Jaikaew <jnattawat@kiragames.com>
2016-06-05 19:48:53 +0700

    Test

--- app/settings/installer.sh
+++ app/settings/installer.sh
@@ -2,4 +2,5 @@ if [ ! -d "sails" ]; then
 	exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:../settings/settings.js
 else 
 	echo Please command sails lift!
+	exec /bin/bash
 fi
