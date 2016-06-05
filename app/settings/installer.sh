50c9124e9a721947236dbc40063aaa88afd571a5
Nattawat Jaikaew <jnattawat@kiragames.com>
2016-06-06 02:03:18 +0700

    Change second Run logic

--- app/settings/installer.sh
+++ app/settings/installer.sh
@@ -2,7 +2,6 @@ if [ ! -d "sails" ]; then
 	exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:../settings/settings.js
 	exec supervisord -n
 else 
-	exec rm -rf sails
-	exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:../settings/settings.js
+	echo 'Please execute container in background'
 	exec supervisord -n
 fi
