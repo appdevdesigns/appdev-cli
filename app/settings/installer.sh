6c947abfede8f9a6cd755ad32bf4817dcbba58b6
Nattawat Jaikaew <jnattawat@kiragames.com>
2016-06-06 01:58:04 +0700

    Add supervisord

--- app/settings/installer.sh
+++ app/settings/installer.sh
@@ -1,6 +1,8 @@
 if [ ! -d "sails" ]; then
 	exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:../settings/settings.js
+	exec supervisord -n
 else 
-	echo Please command sails lift!
+	exec rm -rf sails
+	exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:../settings/settings.js
 	exec supervisord -n
 fi
