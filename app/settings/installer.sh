57d5552ea3fa6452ac98b526129af3e896cea219
kidlovess <stdsitsuwan2008@gmail.com>
2016-10-22 13:18:27 +0700

    remove add builder

--- app/settings/installer.sh
+++ app/settings/installer.sh
@@ -1,8 +1,7 @@
 if [ ! -d "sails" ]; then
-	exec appdev install sails --docker 'plugins:appdevdesigns/[app_builder]' settings:/src/app/settings/settings.js
-  	exec supervisord -n
- else 
-  	echo "Please execute container in background"
-  	exec supervisord -n
-
+ exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:/src/app/settings/settings.js
+ exec supervisord -n
+ else
+  echo "Please execute container in background"
+ exec supervisord -n
 fi
