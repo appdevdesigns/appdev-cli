0f4eaac3f030a6e18989266f66d0651eb68f91e0
kidlovess <stdsitsuwan2008@gmail.com>
2016-10-22 11:31:10 +0700

    add "

--- app/settings/installer.sh
+++ app/settings/installer.sh
@@ -1,8 +1,8 @@
 if [ ! -d "sails" ]; then
 	exec appdev install sails --docker 'plugins:appdevdesigns/[app_builder]' settings:/src/app/settings/settings.js
   	exec supervisord -n
-  else 
-  	echo 'Please execute container in background'
+ else 
+  	echo "Please execute container in background"
   	exec supervisord -n
 
 fi
