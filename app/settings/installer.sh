6fbadab3dbf2a7432e826ddaa7f17fc7b09f233e
kidlovess <stdsitsuwan2008@gmail.com>
2016-10-24 01:29:31 -0700

    add

--- app/settings/installer.sh
+++ app/settings/installer.sh
@@ -1,7 +1,6 @@
 if [ ! -d "sails" ] ; then
  exec appdev install sails --docker 'plugins:appdevdesigns/[fcf_core#develop|fcf_activities#develop|opstool-process-reports|opstool-process-approval#develop|opstool-process-translation|opstool-emailNotifications#develop|fcf_activity_manager]' settings:/src/app/settings/settings.js
  exec supervisord -n
- 
  else
  echo 'Please execute container in background'
  exec supervisord -n
